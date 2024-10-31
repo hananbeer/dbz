190a4fd719d24cd40ff526d7bb401681f8bd11c1
high_byte <email@hanan.beer>
2024-10-31 21:59:20 +0200

    option to get inverse signal ie the acapella

--- zen.py
+++ zen.py
@@ -1,60 +1,19 @@
-import os
-import gc
-import time
 import onnx
 import torch
 import numpy as np
-import librosa
-import platform
 import warnings
-import soundfile
 import onnxruntime
 
 from onnx2pytorch import ConvertModel
 
 warnings.filterwarnings("ignore", "(The given NumPy array is not writable|PySoundFile failed|To copy construct from a tensor)")
 
-if platform.system() == "Darwin":
-    is_windows = False
-    is_macos = True
-elif platform.system() == "Linux":
-    is_windows = False
-    is_macos = False
-elif platform.system() == "Windows":
-    is_windows = True
-    is_macos = False
-
-def clear_gpu_cache():
-    gc.collect()
-    if is_macos:
-        torch.backends.mps.empty_cache()
-    else:
-        torch.cuda.empty_cache()
-
-def stft(signal, n_fft, hop_length):
-    # Create a Hann window
-    window = np.hanning(n_fft)
-    # Calculate the number of frames
-    num_frames = 1 + (len(signal) - n_fft) // hop_length
-    # Pad the signal
-    padded_signal = np.pad(signal, (0, n_fft), mode='constant')
-    # Initialize the STFT matrix
-    stft_matrix = np.zeros((num_frames, n_fft), dtype=complex)
-
-    for i in range(num_frames):
-        start = i * hop_length
-        frame = padded_signal[start:start + n_fft] * window
-        stft_matrix[i, :] = np.fft.fft(frame)
-
-    return stft_matrix
-
 class STFT:
     def __init__(self, n_fft, hop_length, dim_f, device):
         self.n_fft = n_fft
         self.hop_length = hop_length
-        #self.window = torch.hann_window(window_length=self.n_fft, periodic=True)
-        # using numpy instead: (does not result in exact output, but like 0.1% difference)
-        self.window = torch.tensor(np.hanning(self.n_fft).astype(np.float32))
+        # self.window = torch.hann_window(window_length=self.n_fft, periodic=True)
+        self.window = torch.tensor(np.hanning(self.n_fft).astype(np.float32)).to(device)
         self.dim_f = dim_f
         self.device = device
 
@@ -106,44 +65,28 @@ class ZenDemixer:
     hop = 1024
     dim_f = 3072
     dim_t = 2 ** 8
-    mdx_stem_count = 1
-    compensate = 1.022
     n_fft = 6144
-    mdx_segment_size = 1024
-
-    # samplerate = 44100
-    buffer_ms = None # 1500
+    mdx_segment_size = 256
+    chunk_size = hop * (mdx_segment_size - 1)
 
     def __init__(self, use_gpu=True):
-        if is_macos and use_gpu and torch.backends.mps.is_available():
+        if use_gpu and torch.backends.mps.is_available():
             self.device = 'mps'
         elif use_gpu and torch.cuda.is_available():
             self.device = 'cuda'
         else:
             self.device = 'cpu'
 
-        # taken from the model_data.json
-        #self.max_dim_f_set = 3072
-        #self.max_dim_t_set = 8 # 2**8
-        #self.mdx_n_fft_scale_set = 6144
-        
-        self.chunk_size = self.hop * (self.mdx_segment_size - 1)
-        self.stft = STFT(self.n_fft, self.hop, self.dim_f, self.device)
+        # TODO: should keep stft in cpu? seems just as fast or faster
+        self.stft = STFT(self.n_fft, self.hop, self.dim_f, 'cpu') # self.device)
         self._load_model()
 
     def _load_model(self):
-        print('loading model')
+        provider = 'CUDAExecutionProvider' if self.device == 'cuda' else 'CPUExecutionProvider'
+        inference = onnxruntime.InferenceSession(self.model_path, providers=[provider])
+        self.model = lambda spec: inference.run(None, {'input': spec.cpu().numpy()})[0]
 
-        # TODO: optimize this? 1024 seems to be faster, even though it needs to ConvertModel()
-        if self.mdx_segment_size == self.dim_t and self.device != 'mps':
-            provider = 'CUDAExecutionProvider' if self.device == 'cuda' else 'CPUExecutionProvider'
-            inference = onnxruntime.InferenceSession(self.model_path, providers=[provider])
-            self.model = lambda spek: inference.run(None, {'input': spek.cpu().numpy()})[0]
-        else:
-            self.model = ConvertModel(onnx.load(self.model_path))
-            self.model.to(self.device).eval()
-
-    def run_model(self, mix, adjust=1.0):
+    def run_model(self, mix, adjust=1.0, inverse=False):
         """
         expects `mix` to be a `torch.tensor` of shape (batch_size=1, channels=2, samples=self.chunk_size)
         (batch sizes larger than 1 are not supported, model was trained on channels=2 and chunk_size=(1024*1023))
@@ -152,14 +95,18 @@ class ZenDemixer:
         if adjust != 1.0:
             spec *= adjust
 
-        spec[:, :, :3, :] *= 0 
+        # spec[:, :, :3, :] *= 0
+        # NOTE: onnx supports batch_size > 1, demo:
+        # spec = torch.cat([spec, spec], dim=0)
         with torch.no_grad():
-            spec_pred = self.model(spec)
+            spec_pred = self.model(spec)#[:1, ...]
 
         tensor = torch.tensor(spec_pred).to(self.device)
+        if inverse:
+            tensor = spec - tensor
         return self.stft.inverse(tensor).cpu().detach().numpy()
 
-    def demix(self, mix, buffer_size=None):
+    def demix(self, mix, buffer_size=None, progress_cb=None, inverse=False):
         """
         the model always processes chunks of self.chunk_size samples
 
@@ -190,60 +137,17 @@ class ZenDemixer:
             assert buffer_size <= chunk_size, 'buffer_size must be less than or equal to chunk_size'
 
         for start in range(0, sample_count, buffer_size):
-            stime = time.perf_counter()
-
-            end = start + buffer_size
-            chunk = mix[:, start:end]
-            if chunk_size != buffer_size:
-                padding = np.zeros((channel_count, chunk_size - buffer_size), dtype=np.float32)
+            chunk = mix[:, start:start + buffer_size]
+            end = start + chunk.shape[1]
+            if chunk.shape[1] < chunk_size:
+                padding = np.zeros((channel_count, chunk_size - chunk.shape[1]), dtype=np.float32)
                 chunk = np.concatenate((chunk, padding), axis=-1)
 
             chunk_tensor = torch.tensor(np.array([chunk]), dtype=torch.float32).to(self.device)
-            spec_pred = self.run_model(chunk_tensor, adjust=self.adjust)
+            spec_pred = self.run_model(chunk_tensor, adjust=self.adjust, inverse=inverse)
             result[:, start:end] = spec_pred[..., :buffer_size]
 
-            print('%.2f%% (%.2fs)' % (100. * end / sample_count, time.perf_counter() - stime))
+            if progress_cb:
+                progress_cb(end / sample_count)
 
         return result[:, :sample_count - pad_size]
-
-    def demix_file(self, input_path, output_path):
-        # load waveform
-        # TODO: pass samplerate here?
-        print('loading waveform')
-        mix, samplerate = librosa.load(input_path, mono=False, sr=None)
-        if mix.ndim == 1:
-            mix = np.asfortranarray([mix, mix])
-
-        # run inference
-        print('demixing')
-        source = self.demix(mix)
-
-        # save output
-        print('saving output')
-        soundfile.write(output_path, source.T, samplerate=samplerate)
-
-        clear_gpu_cache()
-
-def process_simple(input_path, output_path):
-    seperator = ZenDemixer(use_gpu=True)
-    seperator.demix_file(input_path, output_path)
-
-
-if __name__ == "__main__":
-    input_path = r'./input/Eminem - Rap God.mp3'
-    # input_path = r'./input/Why iii Love The Moon.mp4'
-    # input_path = r'./input_buffer.wav'
-    output_dir = './output'
-
-    filename = os.path.basename(input_path)
-    output_path = os.path.join(output_dir, f'{filename[:-4]}_Instrumental.wav')
-
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir) 
-
-    stime = time.perf_counter()
-
-    process_simple(input_path, output_path)
-
-    print('done: %.2fs' % (time.perf_counter() - stime))
-
---
+++ zen_from_file.py
@@ -0,0 +1,59 @@
+try:
+    import librosa
+except:
+    print('missing required module librosa, install with:\n\tpip install librosa==0.9.2')
+    exit(1)
+
+import os
+import time
+import numpy as np
+import argparse
+import soundfile
+
+ap = argparse.ArgumentParser()
+ap.add_argument('input', type=str)
+ap.add_argument('output', type=str, nargs='?', default='output.wav')
+ap.add_argument('--gpu', type=bool, default=True)
+ap.add_argument('--inverse', action='store_true', default=False)
+
+args = ap.parse_args()
+
+input_path = os.path.abspath(args.input)
+output_path = os.path.abspath(args.output)
+output_dir = os.path.dirname(output_path)
+
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir) 
+
+# load model
+print('loading model...', end=' ')
+
+stime = time.perf_counter()
+show_time = lambda: print('%.2fs' % (time.perf_counter() - stime))
+
+import zen
+separator = zen.ZenDemixer(use_gpu=args.gpu)
+show_time()
+
+# load audio
+print('loading audio...', end=' ')
+stime = time.perf_counter()
+
+mix, samplerate = librosa.load(input_path, mono=False, sr=None)
+if mix.ndim == 1:
+    mix = np.asfortranarray([mix, mix])
+ 
+show_time()
+
+# run inference
+print('demixing........', end='\r')
+stime = time.perf_counter()
+source = separator.demix(mix, progress_cb=lambda p: print('demixing........ %.2f%%' % (100.*p), end='\r'), inverse=args.inverse)
+print('demixing........', end='     ')
+show_time()
+
+# save output
+print('saving output...', end=' ')
+stime = time.perf_counter() 
+soundfile.write(output_path, source.T, samplerate=samplerate)
+show_time()
