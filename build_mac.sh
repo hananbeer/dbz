b98dc899c5b54703d4c20ca007f9dc541a1d3914
Hanan Beer <email@hanan.beer>
2024-11-13 19:39:04 +0200

    mac build script, but still it seems to only work from terminal

--- build_mac.sh
+++ build_mac.sh
@@ -1,6 +1,6 @@
 pyinstaller zen_mode.py -y --exclude-module pkg_resources --icon=img/zen-256.ico --hidden-import pyaudio --collect-submodules pyaudio
 pyinstaller zen_mode.spec -y
-mkdir -p dist/img
-cp img/zen-256.png dist/img/.
-mkdir -p dist/models
-cp models/UVR-MDX-NET-Inst_HQ_3.onnx dist/models/.
+mkdir -p dist/zen_mode/img
+cp img/zen-256.png dist/zen_mode/img/.
+mkdir -p dist/zen_mode/models
+cp models/UVR-MDX-NET-Inst_HQ_3.onnx dist/zen_mode/models/.
